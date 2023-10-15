232b7463db6f3592d3fe3c34ded4941470b33725
Mutiulrasool <138636782+Mutiulrasool@users.noreply.github.com>
2023-10-14 22:51:59 -0700

    3rd Assignment Is Done.

---
+++ app.js
@@ -0,0 +1,22 @@
+var age= 15
+console.log(age)
+alert("My age is 15 years")
+
+var visitedTimes= 14
+console.log(visitedTimes)
+alert("youn have visited this site 14 Times")
+
+var birthYear= 2003
+console.log(birthYear)
+alert("My birth Year is 2023\nType of My declared variable is Varibles for Numbers")
+
+var visiterName= "Muhammad Ali"
+console.log(visiterName)
+
+var productTitle= "Banaspati"
+console.log(productTitle)
+
+var quantTity= "5kg"
+console.log(quantTity)
+
+alert("Muhammad Ali ordered 10 T-shirts on XYZ Clothing store")
